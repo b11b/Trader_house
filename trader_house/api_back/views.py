3544638a9ec9cde1d9d92253fa4417ba48c815ee
Mikhail <mlxxney@gmail.com>
2024-11-11 19:18:24 +0300

    add api

--- trader_house/api_back/views.py
+++ trader_house/api_back/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreate(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
